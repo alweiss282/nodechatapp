3aa2754354f6e946d35e685f6600aef0d454fd58
Alex Weiss <ajw282@cornell.edu>
2022-11-02 13:29:58 -0400

    Added list of active users.

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-var express = require('express')
-var html = require('http')
-
-var app = express()
-var server = html.createServer(app)
-
-var io = require('socket.io')(server)
-var path = require('path')
+const express = require('express')
+const http = require('http')
+const app = express()
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server)
+const path = require('path')
 
 app.use(express.static(path.join(__dirname, './public')));
-
-var name;
+const names = []
 
 io.on('connection', (socket) => {
   console.log('new user connected')
+  let name;
 
   socket.on('joining msg', (username) => {
     name = username
+    names.push(name)
     io.emit('chat message', `---${name} joined the chat---`)
+    io.emit('update names', names)
   })
 
   socket.on('disconnect', () => {
     console.log('user disconnected')
+    names.splice(names.indexOf(name), 1)
     io.emit('chat message', `---${name} left the chat---`)
+    io.emit('update names', names)
   })
 
   socket.on('chat message', (msg) => {
